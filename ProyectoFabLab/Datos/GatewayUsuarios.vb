fab54d45a5f5ef74ef70ae6b5bce4c71f1864e22
miguiX <miguix1997@gmail.com>
2017-11-29 12:48:54 +0100

    Insertar

--- ProyectoFabLab/Datos/GatewayUsuarios.vb
+++ ProyectoFabLab/Datos/GatewayUsuarios.vb
@@ -12,7 +12,14 @@ Public Class GatewayUsuaios
     Public Function Insertar(id As Integer, nombre As String, apellidos As String, fecha_nacimiento As Date, email As String, direccion As String, Organizacion As String, tipo As Integer, fecha_alta As Date)
         Dim Consulta
 
-        Consulta = "INSERT Into Usuarios (id,nombre,apellidos, fecha_nacimiento, email, direccion, Organizacion, tipo, fecha_alta) VALUES (@id, @nombre, @apellidos, @fecha_nacimiento, @email, @direccion, @Organizacion, @tipo, @fecha_alta"
+        Consulta = String.Format("INSERT Into Usuarios (id,nombre,apellidos, fecha_nacimiento, email, direccion, Organizacion, tipo, fecha_alta) VALUES ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}')", id, nombre, apellidos, fecha_nacimiento, email, direccion, Organizacion, tipo, fecha_alta)
 
+        If nombre = "" Or nombre Is Nothing Then
+            Throw New ArgumentException("El nombre no puede estar vacío")
+        End If
+
+        If apellidos = "" Or apellidos Is Nothing Then
+            Throw New ArgumentException("Los apellidos no puede estar vacío")
+        End If
     End Function
 End Class
