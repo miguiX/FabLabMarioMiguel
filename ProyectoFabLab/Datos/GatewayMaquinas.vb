dd620e4c412ccb94279e8993dfbade28a8c0ba0a
Mario <mario_draco@hotmail.es>
2017-12-04 12:55:00 +0100

    Gateway usuarios actuallizado

--- ProyectoFabLab/Datos/GatewayMaquinas.vb
+++ ProyectoFabLab/Datos/GatewayMaquinas.vb
@@ -1,3 +1,23 @@
-﻿Public Class GatewayMaquinas
+﻿Imports System.Data.SqlClient
+''' <summary>
+''' Clase que define un gateway para la tabla Maquinas
+''' </summary>
 
+Public Class GatewayMaquinas
+    ''' <summary>
+    ''' Almacena el objecto Connection
+    ''' </summary>
+    Private conexion As SqlConnection
+    ''' <summary>
+    ''' Almacena el objeto Command
+    ''' </summary>
+    Private comando As SqlCommand
+    Public Sub New(ByRef cadenaConexion As String)
+        conexion = New SqlConnection(cadenaConexion)
+        comando = New SqlCommand
+        comando.Connection = conexion
+    End Sub
+    Public Function Insertar(id As Integer, modelo As String, precio_hora As Double, fecha_compra As String)
+
+    End Function
 End Class
