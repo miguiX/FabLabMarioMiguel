f3323fa5d590fe561cfb32b7afd0ac557007a393
miguiX <miguix1997@gmail.com>
2017-12-04 10:41:26 +0100

    eliminado stringformat

--- ProyectoFabLab/Datos/GatewayUsuarios.vb
+++ ProyectoFabLab/Datos/GatewayUsuarios.vb
@@ -9,25 +9,38 @@ Public Class GatewayUsuaios
         comando = New SqlCommand
     End Sub
 
-    Public Function Insertar(id As Integer, nombre As String, apellidos As String, fecha_nacimiento As Date, email As String, direccion As String, Organizacion As String, tipo As Integer, fecha_alta As Date)
+    Public Function Insertar(id As Integer, nombre As String, apellidos As String, fecha_nacimiento As Date, telefono As String, email As String, direccion As String, Organizacion As String, tipo As Integer, fecha_alta As Date)
         Dim filas As Integer
         Dim Consulta
 
-        Consulta = String.Format("INSERT Into Usuarios (id,nombre,apellidos, fecha_nacimiento, email, direccion, Organizacion, tipo, fecha_alta) VALUES ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}')", id, nombre, apellidos, fecha_nacimiento, email, direccion, Organizacion, tipo, fecha_alta)
-
         If nombre = "" Or nombre Is Nothing Then
             Throw New ArgumentException("El nombre no puede estar vacío")
+        Else
+            comando.Parameters.Add("@nombre", SqlDbType.VarChar)
+            comando.Parameters("@nombre").Value = nombre
         End If
 
         If apellidos = "" Or apellidos Is Nothing Then
             Throw New ArgumentException("Los apellidos no puede estar vacío")
+        Else
+            comando.Parameters.Add("@apellidos", SqlDbType.VarChar)
+            comando.Parameters("@apellidos").Value = apellidos
         End If
-        If fecha_nacimiento = "" Then
+        If fecha_nacimiento = DateTime.MinValue Then
             Throw New ArgumentException("La fecha_nacimiento no puede estar vacía")
+        Else
+            comando.Parameters.Add("@fecha_nacimiento", SqlDbType.Date)
+            comando.Parameters("@fecha_nacimiento").Value = fecha_nacimiento
         End If
-        If fecha_nacimiento = "" Then
+        If telefono = "" Or telefono Is Nothing Then
             Throw New ArgumentException("La fecha_nacimiento no puede estar vacía")
+        Else
+            comando.Parameters.Add("@telefono", SqlDbType.VarChar)
+            comando.Parameters("@telefono").Value = telefono
         End If
+
+        Consulta = "INSERT INTO Usuarios(nombre,apellidos,fecha_nacimiento,email,direccion,organizacion,tipo,fecha_alta)" &
+            "VALUES(@nombre,@apellidos,@fecha_nacimiento,@telefono,@email,@direccion,@organizacion,@tipo,@fecha_alta)"
 
         Try
             conexion.Open()
