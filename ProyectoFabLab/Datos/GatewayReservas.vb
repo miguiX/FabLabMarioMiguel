c953f9ae6a18d8b1dd2caa18e66e916ffdb8ddd9
miguiX <miguix1997@gmail.com>
2017-12-14 18:27:09 +0100

    GatewayReservas y TiposUsuario terminados

--- ProyectoFabLab/Datos/GatewayReservas.vb
+++ ProyectoFabLab/Datos/GatewayReservas.vb
@@ -9,11 +9,92 @@ Public Class GatewayReservas
         comando = New SqlCommand
     End Sub
 
-    Public Function Insertar(fecha As Date, hora As Timer, Usuario As Integer, proyecto As String) As Integer
+    Public Function Insertar(fecha As Date, hora As Timer, usuario As Integer, proyecto As String) As Integer
         Dim filas As Integer
         Dim Consulta As String
 
-        Consulta = String.Format("INSERT INTO Reservas(fecha,hora,usuarios,proyecto")
+        Consulta = String.Format("INSERT INTO Reservas(fecha={0},hora={1},usuarios={2},proyecto={3}", fecha, hora, usuario, proyecto)
+        Try
+            conexion.Open()
+            comando.CommandText = Consulta
+            filas = comando.ExecuteNonQuery
+        Catch ex As Exception
+            Throw New Exception(ex.Message, ex)
+        Finally
+            If (conexion IsNot Nothing) Then
+                conexion.Close()
+            End If
+        End Try
+
+        Return filas
     End Function
 
+    Public Function Actualizar(id As Integer, fecha As Date, hora As Timer, usuario As Integer, proyecto As String) As Integer
+        Dim filas As Integer
+        Dim Consulta As String
+
+        If id = 0 Then
+            Throw New ArgumentException("El id no puede ser cero")
+        End If
+
+        Consulta = String.Format("UPDATE Reservas SET fecha={0},hora={1},usuario={2},proyecto={3} WHERE id={4}", fecha, hora, usuario, proyecto, id)
+        Try
+            conexion.Open()
+            comando.CommandText = Consulta
+            filas = comando.ExecuteNonQuery
+        Catch ex As Exception
+            Throw New Exception(ex.Message, ex)
+        Finally
+            If (conexion IsNot Nothing) Then
+                conexion.Close()
+            End If
+        End Try
+
+        Return filas
+    End Function
+
+    Public Function Eliminar(id As Integer) As Integer
+        Dim filas As Integer
+        Dim consulta As String = String.Format("DELETE FROM Reservas WHERE id={0}", id)
+
+        If id = 0 Then
+            Throw New ArgumentException("El id no puede ser cero")
+        End If
+
+        Try
+            conexion.Open()
+            comando.CommandText = consulta
+            filas = comando.ExecuteNonQuery
+        Catch ex As Exception
+            Throw New Exception(ex.Message, ex)
+        Finally
+            If (conexion IsNot Nothing) Then
+                conexion.Close()
+            End If
+        End Try
+
+        Return filas
+    End Function
+    Public Function SeleccionarTodo() As DataTable
+        Dim Consulta As String
+        Dim todo As New DataTable
+        Dim reader As SqlDataReader
+
+        Consulta = String.Format("SELECT * FROM Reservas")
+
+        Try
+            conexion.Open()
+            comando.CommandText = Consulta
+            reader = comando.ExecuteReader()
+
+            todo.Load(reader)
+        Catch ex As Exception
+            Throw New Exception(ex.Message, ex)
+        Finally
+            If (conexion IsNot Nothing) Then
+                conexion.Close()
+            End If
+        End Try
+        Return todo
+    End Function
 End Class
--- ProyectoFabLab/Datos/GatewayTipoUsuario.vb
+++ ProyectoFabLab/Datos/GatewayTipoUsuario.vb
@@ -1,3 +1,103 @@
-﻿Public Class GatewayTipoUsuario
+﻿Imports System.Data.SqlClient
 
+Public Class GatewayTipoUsuario
+    Dim conexion As SqlConnection
+    Dim comando As New SqlCommand
+
+    Public Sub New(ByRef cadenaConexion As String)
+        conexion = New SqlConnection(cadenaConexion)
+        comando = New SqlCommand
+    End Sub
+
+    Public Function Insertar(tipo As String) As Integer
+        Dim filas As Integer
+        Dim Consulta As String
+
+        Consulta = String.Format("INSERT INTO TiposUsuario(tipo={0})", tipo)
+
+        Try
+            conexion.Open()
+            comando.CommandText = Consulta
+            filas = comando.ExecuteNonQuery
+        Catch ex As Exception
+            Throw New Exception(ex.Message, ex)
+        Finally
+            If (conexion IsNot Nothing) Then
+                conexion.Close()
+            End If
+        End Try
+
+        Return filas
+    End Function
+
+    Public Function Actualizar(id As Integer, tipo As String) As Integer
+        Dim filas As Integer
+        Dim Consulta As String
+
+        If id = 0 Then
+            Throw New ArgumentException("El id no puede ser cero")
+        End If
+
+        Consulta = String.Format("UPDATE TiposUsuario SET tipo={0} WHERE id={1}", tipo, id)
+
+        Try
+            conexion.Open()
+            comando.CommandText = Consulta
+            filas = comando.ExecuteNonQuery
+        Catch ex As Exception
+            Throw New Exception(ex.Message, ex)
+        Finally
+            If (conexion IsNot Nothing) Then
+                conexion.Close()
+            End If
+        End Try
+
+        Return filas
+    End Function
+
+    Public Function Eliminar(id As Integer) As Integer
+        Dim filas As Integer
+        Dim consulta As String = String.Format("DELETE FROM TiposUsuario WHERE id={0}", id)
+
+        If id = 0 Then
+            Throw New ArgumentException("El id no puede ser cero")
+        End If
+
+        Try
+            conexion.Open()
+            comando.CommandText = consulta
+            filas = comando.ExecuteNonQuery
+        Catch ex As Exception
+            Throw New Exception(ex.Message, ex)
+        Finally
+            If (conexion IsNot Nothing) Then
+                conexion.Close()
+            End If
+        End Try
+
+        Return filas
+    End Function
+
+    Public Function SeleccionarTodo() As DataTable
+        Dim Consulta As String
+        Dim todo As New DataTable
+        Dim reader As SqlDataReader
+
+        Consulta = String.Format("SELECT * FROM TiposUsuario")
+
+        Try
+            conexion.Open()
+            comando.CommandText = Consulta
+            reader = comando.ExecuteReader()
+
+            todo.Load(reader)
+        Catch ex As Exception
+            Throw New Exception(ex.Message, ex)
+        Finally
+            If (conexion IsNot Nothing) Then
+                conexion.Close()
+            End If
+        End Try
+        Return todo
+    End Function
 End Class
