bde83710bd3676f793381bb5426172f72096b117
miguiX <miguix1997@gmail.com>
2017-12-14 18:48:35 +0100

    Principal y Pantalla Bienvenida actualizados

--- ProyectoFabLab/Interfaz/PantallaBienvenida.vb
+++ ProyectoFabLab/Interfaz/PantallaBienvenida.vb
@@ -1,7 +1,5 @@
 ﻿Public Class PantallaBienvenida
     Private Sub PantallaBienvenida_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim gateWayUsuarios As New GatewayUsuarios(My.Settings.cadenaConexion)
 
-        gateWayUsuarios.ActualizarBaseDatosUsuarios()
     End Sub
 End Class
--- ProyectoFabLab/Interfaz/Principal.vb
+++ ProyectoFabLab/Interfaz/Principal.vb
@@ -1,3 +1,8 @@
 ﻿Public Class Principal
-
+    Private Sub Principal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim NuevoForm As New PantallaBienvenida
+        NuevoForm.MdiParent = Me
+        NuevoForm.Text = "Pantalla Bienvenida - FabLab"
+        NuevoForm.Show()
+    End Sub
 End Class
