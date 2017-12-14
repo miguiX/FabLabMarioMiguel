d8b79f1ed7c828d9266cb18652bcb7e663e84571
miguiX <miguix1997@gmail.com>
2017-12-14 18:09:24 +0100

    Gateway Reservas empezado

--- ProyectoFabLab/Datos/GatewayReservas.vb
+++ ProyectoFabLab/Datos/GatewayReservas.vb
@@ -1,3 +1,19 @@
-﻿Public Class GatewayReservas
+﻿Imports System.Data.SqlClient
+
+Public Class GatewayReservas
+    Dim conexion As SqlConnection
+    Dim comando As New SqlCommand
+
+    Public Sub New(ByRef cadenaConexion As String)
+        conexion = New SqlConnection(cadenaConexion)
+        comando = New SqlCommand
+    End Sub
+
+    Public Function Insertar(fecha As Date, hora As Timer, Usuario As Integer, proyecto As String) As Integer
+        Dim filas As Integer
+        Dim Consulta As String
+
+        Consulta = String.Format("INSERT INTO Reservas(fecha,hora,usuarios,proyecto")
+    End Function
 
 End Class
