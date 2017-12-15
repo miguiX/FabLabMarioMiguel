f6a1a8d36dbfa6a51245ed4f72c6a6bd8f53795c
miguiX <miguix1997@gmail.com>
2017-12-15 12:45:11 +0100

    Gestion de usuarios actualizada

--- ProyectoFabLab/Interfaz/GestionUsuarios.Designer.vb
+++ ProyectoFabLab/Interfaz/GestionUsuarios.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class GestionUsuarios
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,18 +20,153 @@ Partial Class GestionUsuarios
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.LabelBuscar = New System.Windows.Forms.Label()
+        Me.TextBoxBuscar = New System.Windows.Forms.TextBox()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.DataGridViewUsuarios = New System.Windows.Forms.DataGridView()
+        Me.ButtonNuevoUsuario = New System.Windows.Forms.Button()
+        Me.ButtonEliminar = New System.Windows.Forms.Button()
+        Me.ButtonEditar = New System.Windows.Forms.Button()
+        Me.ButtonConsultar = New System.Windows.Forms.Button()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.FlowLayoutPanel1.SuspendLayout()
+        CType(Me.DataGridViewUsuarios, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 3
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.52632!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 36.84211!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 52.63158!))
+        Me.TableLayoutPanel1.Controls.Add(Me.LabelBuscar, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.TextBoxBuscar, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.ButtonNuevoUsuario, 2, 0)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.TableLayoutPanel1.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 1
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(957, 53)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'LabelBuscar
+        '
+        Me.LabelBuscar.AutoSize = True
+        Me.LabelBuscar.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.LabelBuscar.Location = New System.Drawing.Point(3, 0)
+        Me.LabelBuscar.Name = "LabelBuscar"
+        Me.LabelBuscar.Size = New System.Drawing.Size(94, 53)
+        Me.LabelBuscar.TabIndex = 0
+        Me.LabelBuscar.Text = "Buscar"
+        Me.LabelBuscar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'TextBoxBuscar
+        '
+        Me.TextBoxBuscar.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBoxBuscar.Location = New System.Drawing.Point(103, 15)
+        Me.TextBoxBuscar.Margin = New System.Windows.Forms.Padding(3, 15, 3, 3)
+        Me.TextBoxBuscar.Name = "TextBoxBuscar"
+        Me.TextBoxBuscar.Size = New System.Drawing.Size(346, 22)
+        Me.TextBoxBuscar.TabIndex = 1
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.Controls.Add(Me.ButtonEliminar)
+        Me.FlowLayoutPanel1.Controls.Add(Me.ButtonEditar)
+        Me.FlowLayoutPanel1.Controls.Add(Me.ButtonConsultar)
+        Me.FlowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.FlowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(0, 392)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(957, 57)
+        Me.FlowLayoutPanel1.TabIndex = 1
+        '
+        'DataGridViewUsuarios
+        '
+        Me.DataGridViewUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridViewUsuarios.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridViewUsuarios.Location = New System.Drawing.Point(0, 53)
+        Me.DataGridViewUsuarios.Name = "DataGridViewUsuarios"
+        Me.DataGridViewUsuarios.RowTemplate.Height = 24
+        Me.DataGridViewUsuarios.Size = New System.Drawing.Size(957, 339)
+        Me.DataGridViewUsuarios.TabIndex = 2
+        '
+        'ButtonNuevoUsuario
+        '
+        Me.ButtonNuevoUsuario.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ButtonNuevoUsuario.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ButtonNuevoUsuario.Location = New System.Drawing.Point(805, 3)
+        Me.ButtonNuevoUsuario.Name = "ButtonNuevoUsuario"
+        Me.ButtonNuevoUsuario.Size = New System.Drawing.Size(149, 47)
+        Me.ButtonNuevoUsuario.TabIndex = 2
+        Me.ButtonNuevoUsuario.Text = "Nuevo Usuario"
+        Me.ButtonNuevoUsuario.UseVisualStyleBackColor = True
+        '
+        'ButtonEliminar
+        '
+        Me.ButtonEliminar.AutoSize = True
+        Me.ButtonEliminar.Dock = System.Windows.Forms.DockStyle.Right
+        Me.ButtonEliminar.Location = New System.Drawing.Point(886, 3)
+        Me.ButtonEliminar.Margin = New System.Windows.Forms.Padding(20, 3, 3, 3)
+        Me.ButtonEliminar.Name = "ButtonEliminar"
+        Me.ButtonEliminar.Size = New System.Drawing.Size(68, 27)
+        Me.ButtonEliminar.TabIndex = 0
+        Me.ButtonEliminar.Text = "Eliminar"
+        Me.ButtonEliminar.UseVisualStyleBackColor = True
+        '
+        'ButtonEditar
+        '
+        Me.ButtonEditar.Dock = System.Windows.Forms.DockStyle.Right
+        Me.ButtonEditar.Location = New System.Drawing.Point(748, 3)
+        Me.ButtonEditar.Margin = New System.Windows.Forms.Padding(20, 3, 3, 3)
+        Me.ButtonEditar.Name = "ButtonEditar"
+        Me.ButtonEditar.Size = New System.Drawing.Size(115, 27)
+        Me.ButtonEditar.TabIndex = 1
+        Me.ButtonEditar.Text = "Editar"
+        Me.ButtonEditar.UseVisualStyleBackColor = True
+        '
+        'ButtonConsultar
+        '
+        Me.ButtonConsultar.Dock = System.Windows.Forms.DockStyle.Right
+        Me.ButtonConsultar.Location = New System.Drawing.Point(597, 3)
+        Me.ButtonConsultar.Margin = New System.Windows.Forms.Padding(20, 3, 3, 3)
+        Me.ButtonConsultar.Name = "ButtonConsultar"
+        Me.ButtonConsultar.Size = New System.Drawing.Size(128, 27)
+        Me.ButtonConsultar.TabIndex = 2
+        Me.ButtonConsultar.Text = "Consultar"
+        Me.ButtonConsultar.UseVisualStyleBackColor = True
         '
         'GestionUsuarios
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(957, 449)
+        Me.Controls.Add(Me.DataGridViewUsuarios)
+        Me.Controls.Add(Me.FlowLayoutPanel1)
+        Me.Controls.Add(Me.TableLayoutPanel1)
         Me.Name = "GestionUsuarios"
         Me.Text = "Form1"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        Me.FlowLayoutPanel1.ResumeLayout(False)
+        Me.FlowLayoutPanel1.PerformLayout()
+        CType(Me.DataGridViewUsuarios, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents DataGridViewUsuarios As DataGridView
+    Friend WithEvents LabelBuscar As Label
+    Friend WithEvents TextBoxBuscar As TextBox
+    Friend WithEvents ButtonNuevoUsuario As Button
+    Friend WithEvents ButtonEliminar As Button
+    Friend WithEvents ButtonEditar As Button
+    Friend WithEvents ButtonConsultar As Button
 End Class
