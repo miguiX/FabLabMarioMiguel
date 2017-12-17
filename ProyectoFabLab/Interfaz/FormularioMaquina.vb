cae0932d163c66bbf1a84261fb50dc13cf542524
Mario <mario.juarez16@iesdoctorbalmis.com>
2017-12-17 17:25:09 +0100

    imagen subuda y elementos actualizados

--- ProyectoFabLab/Interfaz/FormularioMaquina.vb
+++ ProyectoFabLab/Interfaz/FormularioMaquina.vb
@@ -1,3 +1,7 @@
 ﻿Public Class FormularioMaquina
-
+    Private Sub FormularioMaquina_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        If MessageBox.Show("¿Esta seguro que desea cerrar la ventana?", "Aviso", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) = 7 Then
+            e.Cancel = True
+        End If
+    End Sub
 End Class
